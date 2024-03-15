845b7c4d8322d96e0e36128b3a66c66f48116b9c
unknown <Kaleeswaran722004@gmail.com>
2024-03-15 13:50:54 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// import logo from './logo.svg';
+// import './App.css';
+import {Route,Routes} from 'react-router-dom';
+import { Navbar } from './component/Navbar';
+import { Login } from './component/Login';
+import {Signup} from './component/Signup';
+import { Auth } from './component/Auth';
+import { Protected } from './component/Protected';
+import Nomatch from './component/Nomatch';
+import Home from './component/Home';
+import Admin from './component/Admin';
+import User from './component/User';
+function App() {
+  return (
+    <div className="App">
+      <Auth>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/*' element={<Nomatch/>}/>
+        <Route path='/users' element={<Protected>
+          <User/>
+        </Protected>}></Route>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/Admin' element={<Protected><Admin/></Protected>
+          
+      }/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+      </Auth>
+    </div>
+  );
+}
+
+export default App;
